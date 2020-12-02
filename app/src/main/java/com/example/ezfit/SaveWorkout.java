36d73334943926689673728ea91758f6608cbbfd
C18742005 <c18742005@mytudublin.ie>
2020-12-02 10:50:58 +0000

    Split track workout into 2 activities to allow one track exercises while the other saves the workout to the database. Makes for a clearer UI for user

--- app/src/main/java/com/example/ezfit/SaveWorkout.java
+++ app/src/main/java/com/example/ezfit/SaveWorkout.java
@@ -1,30 +1,16 @@
 package com.example.ezfit;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TrackWorkout extends AppCompatActivity {
+public class SaveWorkout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.track_workout);
-
-        // Code to control what happens when the save workout button is clicked
-        Button saveButton = (Button) findViewById(R.id.save_workout);
-
-        saveButton.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent switchScreens = new Intent(TrackWorkout.this, SaveWorkout.class);
-                        startActivity(switchScreens);
-                    }
-                }
-        );
+        setContentView(R.layout.save_workout);
 
         // Code to control what happens when the return button is clicked
         Button returnButton = (Button) findViewById(R.id.goBack);
