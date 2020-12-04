50741508f984ccf3c20bb32ed72e71fa9376b947
C18742005 <c18742005@mytudublin.ie>
2020-12-04 14:47:45 +0000

    Added to the Progress Pictures class to allow a user to take a photo of their progress.

--- app/src/main/java/com/example/ezfit/ProgressPictures.java
+++ app/src/main/java/com/example/ezfit/ProgressPictures.java
@@ -1,16 +1,60 @@
 package com.example.ezfit;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class ProgressPictures extends AppCompatActivity {
+    private static final int CAMERA_PERMISSION_CODE = 1;
+    private static final int CAMERA_REQUEST_CODE = 2;
+    ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progress_pictures);
+
+        // Code to control what happens when the capture button is clicked
+        Button captureButton = (Button) findViewById(R.id.takePhoto);
+
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(ContextCompat.checkSelfPermission(ProgressPictures.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Intent to start the camera
+                            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                        } else {
+                            ActivityCompat.requestPermissions(ProgressPictures.this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+                        }
+                    }
+                }
+        );
+
+        // Code to control what happens when the save button is clicked
+        Button saveButton = (Button) findViewById(R.id.savePhoto);
+
+        saveButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                }
+        );
 
         // Code to control what happens when the return button is clicked
         Button returnButton = (Button) findViewById(R.id.goBack);
@@ -23,5 +67,28 @@ public class ProgressPictures extends AppCompatActivity {
                     }
                 }
         );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == CAMERA_PERMISSION_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Intent to start the camera
+            startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        } else {
+            Toast.makeText(ProgressPictures.this, "Permissions for camera must be accepted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        image = (ImageView) findViewById(R.id.imageView);
+
+        if(resultCode == Activity.RESULT_OK && requestCode == CAMERA_REQUEST_CODE) {
+            Bitmap thumbNail = (Bitmap) data.getExtras().get("data");
+            image.setImageBitmap(thumbNail);
+        }
     }
 }
